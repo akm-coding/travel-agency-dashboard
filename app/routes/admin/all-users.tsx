975085401486989e155faf9e1ac30d3a67e96953
Aung Kaung Myat <115795473+akm-coding@users.noreply.github.com>
2025-05-11 15:39:52 +0700

    feat: implement routeing, dashboard layout & UI, auth functionality

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,15 @@
+import { Header } from "components";
 import React from "react";
 
 const AllUsers = () => {
-  return <div>User Table</div>;
+  return (
+    <main className="all-users wrapper">
+      <Header
+        title="Manage Users"
+        description="Filter, sort, and access detailed user profiles"
+      />
+    </main>
+  );
 };
 
 export default AllUsers;
